33778336e03aae4128c877bb2cc7ddb8c2a9940c
sungchan Yeo <sungchan254@naver.com>
2024-05-01 21:55:20 +0900

    django course 29 commit

--- django_pinterest/settings.py
+++ django_pinterest/settings.py
@@ -144,3 +144,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world/')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login/')
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASpiE_DIR, 'media')

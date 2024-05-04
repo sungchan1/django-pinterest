6686c7dd4c6ce4b63bcca930060f2070415b4daf
sungchan Yeo <sungchan254@naver.com>
2024-05-05 00:01:42 +0900

    django course 35 commit

---
+++ articleapp/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+
+from articleapp.models import Article
+
+
+class ArticleCreateForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image', 'content']
+
+
+

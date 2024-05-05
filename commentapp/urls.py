c0ebdc1d15368419edd62a30a3503bdf37bd7e3e
sungchan Yeo <sungchan254@naver.com>
2024-05-05 13:02:13 +0900

    django course 37 commit

---
+++ commentapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from commentapp.views import CommentCreateView
+
+app_name = 'commentapp'
+
+urlpatterns = [
+
+    path('create/', CommentCreateView.as_view(), name='create'),
+
+
+
+]
--- django_pinterest/settings.py
+++ django_pinterest/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'accountapp',
     'profileapp',
     'articleapp',
+    'commentapp',
+
 
 ]
 
